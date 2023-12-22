25c90f63f8a6b05c52003e68ac0db13b9da9bdf5
horizonnns <for.office.gthb@gmail.com>
2023-12-22 11:47:31 +0500

    Made a component for selecting options

---
+++ src/components/ui/BaseSelect.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps({
+	options: {
+		type: Array,
+		required: true,
+	},
+	class: String,
+	modelValue: { type: String, default: null },
+	placeholder: {
+		required: true,
+		type: String,
+	},
+});
+
+const emit = defineEmits(['update']);
+</script>
+
+<template>
+	<div class="flex gap-2">
+		<select
+			:class="class"
+			:placeholder="placeholder"
+			@change="
+				emit(
+					'update:modelValue',
+					$event.target.value
+				)
+			"
+		>
+			<option selected disabled>
+				{{ placeholder }}
+			</option>
+			<option
+				class="bg-white"
+				v-for="{ value, label } in options"
+				:key="value"
+				:value="value"
+				@click="emit('select', label)"
+			>
+				{{ label }}
+			</option>
+		</select>
+	</div>
+</template>
