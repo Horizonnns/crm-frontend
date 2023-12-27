b4a1faa5bd3e0ad12583c8d9edcc57ab4cac2474
Horizonnns <for.office.gthb@gmail.com>
2023-12-27 14:28:48 +0500

    Created an API request function for login

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,11 +1,22 @@
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
 import AppInput from '../../components/ui/AppInput.vue';
 
 const form = ref({
-	username: '',
+	email: '',
 	password: '',
 });
+
+async function login() {
+	await axios
+		.get('http://127.0.0.1:8000/api/login', {
+			params: form.value,
+		})
+		.then((res) => {
+			console.log(res, 'res');
+		});
+}
 </script>
 
 <template>
@@ -14,9 +25,9 @@ const form = ref({
 			<AppInput
 				size="lg"
 				type="text"
-				title="Логин"
-				placeholder="Введите логин"
-				v-model="form.username"
+				title="Email"
+				placeholder="Введите email"
+				v-model="form.email"
 			/>
 			<AppInput
 				size="lg"
@@ -28,6 +39,7 @@ const form = ref({
 		</div>
 
 		<button
+			@click="login"
 			class="w-full duration-300 hover:text-white hover:bg-blue-10 border rounded-md font-bold px-5 py-2 mt-5"
 		>
 			Вход
