374141fe7cef279bbbd0f1b9c7f23c8263bce938
Horizonnns <for.office.gthb@gmail.com>
2024-01-01 18:56:31 +0500

    Installed vuex for data storage

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './assets/css/main.css';
 import App from './App.vue';
 import { vMaska } from 'maska';
 import router from './router';
+import store from './store/store';
 
 createApp(App)
 	.use(router)
+	.use(store)
 	.directive('maska', vMaska)
 	.mount('#app');
