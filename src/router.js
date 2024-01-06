0dfc4cac6a798bf93eeeaf4c2bbdb48f74485ef9
Horizonnns <for.office.gthb@gmail.com>
2024-01-07 01:13:42 +0500

    Fixed the middleware to the frontend for all pages

--- src/router.js
+++ src/router.js
@@ -7,52 +7,72 @@ import Admin from './pages/admin/Admin.vue';
 import FrontOff from './pages/front-off/FrontOff.vue';
 import BackOff from './pages/back-off/BackOff.vue';
 
+const routes = [
+	{
+		name: 'register',
+		path: '/register',
+		component: AuthTabs,
+	},
+	{
+		name: 'admin',
+		path: '/admin',
+		component: Admin,
+		meta: { requiresAuth: true },
+	},
+	{
+		name: 'frontoff',
+		path: '/frontoff',
+		component: FrontOff,
+		meta: { requiresAuth: true },
+	},
+	{
+		name: 'backoff',
+		path: '/backoff',
+		component: BackOff,
+		meta: { requiresAuth: true },
+	},
+];
+
 const router = createRouter({
 	history: createWebHistory(),
-	routes: [
-		{
-			name: 'register',
-			path: '/register',
-			component: AuthTabs,
-		},
-		{
-			name: 'admin',
-			path: '/admin',
-			component: Admin,
-			meta: { requiresAuth: true },
-		},
-		{
-			name: 'frontoff',
-			path: '/frontoff',
-			component: FrontOff,
-			meta: { requiresAuth: true },
-		},
-		{
-			name: 'backoff',
-			path: '/backoff',
-			component: BackOff,
-			meta: { requiresAuth: true },
-		},
-	],
+	routes,
 });
 
-router.beforeEach((to, from, next) => {
-	const isAuthenticated =
+router.beforeEach(async (to, from, next) => {
+	const isAuth =
 		JSON.parse(localStorage.getItem('user')) !==
 		null;
+	const user = JSON.parse(
+		localStorage.getItem('user')
+	);
 
-	if (
-		to.matched.some(
-			(record) => record.meta.requiresAuth
-		)
-	) {
-		if (!isAuthenticated) {
-			next('/register');
+	if (isAuth) {
+		if (to.name === 'register') {
+			next('/admin');
+		} else if (
+			user.role === 'admin' &&
+			to.name !== 'admin'
+		) {
+			next('/admin');
+		} else if (
+			user.role === 'front-office' &&
+			to.name !== 'frontoff'
+		) {
+			next('/frontoff');
+		} else if (
+			user.role === 'back-office' &&
+			to.name !== 'backoff'
+		) {
+			next('/backoff');
 		} else {
 			next();
 		}
 	} else {
-		next();
+		if (to.name !== 'register') {
+			next('/register');
+		} else {
+			next();
+		}
 	}
 });
 
