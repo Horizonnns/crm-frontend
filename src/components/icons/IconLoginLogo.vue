24a7ca3b85d3147504a0473debb4915f9a26b19e
Horizonnns <for.office.gthb@gmail.com>
2024-01-08 23:03:19 +0500

    Entered the logo code into icons

---
+++ src/components/icons/IconLoginLogo.vue
@@ -0,0 +1,14 @@
+<template>
+	<div
+		class="flex items-center bg-white rounded-sm font-black text-xl w-40 px-1.5 py-1 mb-5"
+	>
+		<p class="bg-blue-10 text-white px-5 py-1.5">
+			MINI
+		</p>
+		<p
+			class="text-blue-10 px-5 py-1.5 bg-gray-100"
+		>
+			CRM
+		</p>
+	</div>
+</template>
---
+++ src/components/icons/IconLogo.vue
@@ -0,0 +1,12 @@
+<template>
+	<div
+		class="flex items-center bg-white rounded-sm font-black"
+	>
+		<p
+			class="bg-blue-10 text-white px-5 py-1 my-0.5"
+		>
+			MINI
+		</p>
+		<p class="text-blue-10 pl-4 mr-4">CRM</p>
+	</div>
+</template>
