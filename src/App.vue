5c230592b675c834268a22c1526ba9eae8449c13
Horizonnns <for.office.gthb@gmail.com>
2024-01-09 13:45:25 +0500

    Displayed as a message in the notification when logging in

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
+<script setup>
+import { useStore } from 'vuex';
+import Notify from './components/ui/Notify.vue';
+
+const store = useStore();
+</script>
+
 <template>
 	<section>
+		<div
+			v-show="store.state.token"
+			class="fixed top-20 z-20"
+		>
+			<Notify
+				ifSuccess="Вы успешно вошли в систему!"
+			/>
+		</div>
+
 		<router-view />
 	</section>
 </template>
