45070db094c47164d2c599d010743ab7ebde8db1
Horizonnns <for.office.gthb@gmail.com>
2024-01-13 01:24:03 +0500

    Removed unused shadow from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,6 @@ export default {
 				},
 			},
 
-			boxShadow: {
-				cards: '0 0 12px rgba(51, 51, 51, 0.2)',
-			},
-
 			animation: {
 				'waving-hand':
 					'wave 3s cubic-bezier(.17,.67,.83,.67) 10',
