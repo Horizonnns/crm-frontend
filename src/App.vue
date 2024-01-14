070826560c7f08864c47a6472348425b44f70fa1
Horizonnns <for.office.gthb@gmail.com>
2024-01-14 21:47:24 +0500

    Added Header component in App

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
 import { useStore } from 'vuex';
 import Notify from './components/ui/Notify.vue';
+import Header from './components/Header.vue';
+import { computed } from 'vue';
 
 const store = useStore();
+
+const isAuth = computed(
+	() => store.state.token !== null
+);
 </script>
 
 <template>
 	<section>
+		<Header v-if="isAuth" />
+
 		<Notify class="fixed top-16 z-20" />
 
 		<router-view />
