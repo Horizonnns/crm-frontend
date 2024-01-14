3632a48c794f4ba91504111e92f17aed9f0c9391
Horizonnns <for.office.gthb@gmail.com>
2024-01-14 19:47:36 +0500

    Made modal windows adaptive to create an application on front-office page

--- src/pages/front-off/FrontOff.vue
+++ src/pages/front-off/FrontOff.vue
@@ -680,7 +680,7 @@ const searchApplications = async () => {
 											</div>
 
 											<div
-												class="flex justify-between space-x-5 border-b-2 pb-5"
+												class="flex flex-col sm:flex-row justify-between sm:space-x-5 space-y-5 sm:space-y-0 border-b-2 pb-5"
 											>
 												<div
 													class="w-full space-y-4"
