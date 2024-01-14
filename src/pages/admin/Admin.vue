42b4ffceee8e012a595629ddd93859516140fbe0
Horizonnns <for.office.gthb@gmail.com>
2024-01-14 19:25:43 +0500

    Made a separation of date and time with the "date-fns" package

--- src/pages/admin/Admin.vue
+++ src/pages/admin/Admin.vue
@@ -16,6 +16,7 @@ import IconLogout from '../../components/icons/IconLogout.vue';
 import IconExit from '../../components/icons/IconExit.vue';
 import IconLogo from '../../components/icons/IconLogo.vue';
 import IconProcessing from '../../components/icons/IconProcessing.vue';
+import { format } from 'date-fns';
 
 const store = useStore();
 const router = useRouter();
@@ -252,6 +253,17 @@ async function logOut() {
 		router.push('/register')
 	);
 }
+
+function formatDate(createdAt) {
+	return format(
+		new Date(createdAt),
+		'yyyy-MM-dd'
+	);
+}
+
+function formatTime(createdAt) {
+	return format(new Date(createdAt), 'HH:mm');
+}
 </script>
 
 <template>
@@ -279,10 +291,10 @@ async function logOut() {
 			class="inline-block min-w-full align-middle px-4 mt-6"
 		>
 			<div
-				class="shadow-md px-2 py-3 space-y-4 bg-white overflow-hidden border border-gray-200 md:rounded-lg"
+				class="shadow-md px-2 py-3 space-y-4 bg-white overflow-auto border border-gray-200 rounded-lg"
 			>
 				<div
-					class="flex justify-between items-center px-3 border-b pb-3"
+					class="flex justify-center sm:justify-between items-center px-3 border-b pb-3"
 				>
 					<p class="text-xl font-bold">
 						Список менеджеров
@@ -291,7 +303,7 @@ async function logOut() {
 					<button
 						@click="openModal"
 						type="submit"
-						class="bg-gray-100 hover:bg-gray-200 active:bg-gray-300 duration-200 border rounded-full text-sm font-bold px-4 pt-1.5 pb-2"
+						class="hidden sm:block bg-gray-100 hover:bg-gray-200 active:bg-gray-300 duration-200 border rounded-full text-sm font-bold px-4 pt-1.5 pb-2"
 					>
 						Создать менеджера
 					</button>
@@ -695,57 +707,19 @@ async function logOut() {
 										class="space-y-0.5 text-end"
 									>
 										<p class="text-gray-700">
-								<button
-									@click="deleteUser(user.id)"
-									title="Удалить пользователя"
-									class="rounded-lg text-red-500 p-2 bg-white hover:bg-gray-100 border"
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="1.5"
-										stroke="currentColor"
-										class="w-5 h-5"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-										/>
-									</svg>
-								</button>
-							</td>
-						</tr>
-					</tbody>
-				</table>
+											{{
+												formatTime(
+													user.created_at
+												)
+											}}
 										</p>
 
 										<p class="text-gray-700">
-								<button
-									@click="deleteUser(user.id)"
-									title="Удалить пользователя"
-									class="rounded-lg text-red-500 p-2 bg-white hover:bg-gray-100 border"
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										stroke-width="1.5"
-										stroke="currentColor"
-										class="w-5 h-5"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-										/>
-									</svg>
-								</button>
-							</td>
-						</tr>
-					</tbody>
-				</table>
+											{{
+												formatDate(
+													user.created_at
+												)
+											}}
 										</p>
 
 										<p class="text-gray-400">
