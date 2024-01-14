dd6261afc242f92764e43a4ea30e45ed18bddb46
Horizonnns <for.office.gthb@gmail.com>
2024-01-14 22:15:11 +0500

    Made the logo responsive

--- src/components/icons/IconLogo.vue
+++ src/components/icons/IconLogo.vue
@@ -3,10 +3,10 @@
 		class="flex items-center bg-white rounded-sm font-black"
 	>
 		<p
-			class="bg-blue-10 text-white px-5 py-1 my-0.5"
+			class="bg-blue-10 text-white px-2 sm:px-5 py-1 my-0.5"
 		>
 			MINI
 		</p>
-		<p class="text-blue-10 pl-4 mr-4">CRM</p>
+		<p class="text-blue-10 pl-2 mr-2 sm:pl-4 sm:mr-4">CRM</p>
 	</div>
 </template>
